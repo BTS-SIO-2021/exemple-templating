1dc101c4efa7671d9916bc62316d2ed56663506f
mathilderenversade <mathirenversade@gmail.com>
2021-11-02 10:00:03 +0100

    initial commit

---
+++ template/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <ul>
+        <li><a href="#">Mentions légales</a></li>
+        <li><a href="#">Notre revue de presse</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</footer>
---
+++ template/header.php
@@ -0,0 +1,12 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href='home.php'>Home</a></li>
+            <li><a href='nosvaleurs.php'>Nos valeurs</a></li>
+            <li><a href='infos.php'>Informations</a></li>
+            <li><a href='contact.php'>Contact</a></li>
+            <li><a href="equipe.php">Notre équipe</a></li>
+            <li><a href="https://www.facebook.com">Notre facebook</a></li>
+        </ul> 
+    </nav>
+</header>
